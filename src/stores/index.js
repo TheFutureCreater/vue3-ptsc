b61d5cb98b95c3a72751245cda1d5ac39d22ac44
YwonDong <futurecreaterlan@foxmail.com>
2023-11-25 12:02:56 +0800

    引入pinia

---
+++ src/stores/index.js
@@ -0,0 +1,12 @@
+import { createPinia } from 'pinia'
+import persist from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(persist)
+
+export default pinia
+
+// export * from './modules/user'
+// export * from './modules/job'
+// export * from './modules/state'
+// export * from './modules/admin'
