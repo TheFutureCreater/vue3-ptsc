4b070dc71b15a9a397bd81e4373d739aec9b9e8b
YwonDong <futurecreaterlan@foxmail.com>
2023-12-19 16:42:01 +0800

    逻辑修改

--- src/stores/modules/search.js
+++ src/stores/modules/search.js
@@ -7,14 +7,16 @@ export const useSearchStore = defineStore('ptsc-search-condition', () => {
   const cityName = ref('全国')
   const searchWord = ref('')
 
-  const jobNature = ref([])
-  const jobType = ref([])
-  const business = ref([])
-  const mercNature = ref([])
-  const workAge = ref(0)
-  const mercScale = ref(0)
-  const degree = ref(0)
-  const wageRange = ref(0)
+  const jobFrom = ref([])
+
+  jobFrom.value[0] = 0 // 0学历要求 degree
+  jobFrom.value[1] = [] // 1职位性质 jobNature
+  jobFrom.value[2] = 0 // 2工作年限 workAge
+  jobFrom.value[3] = 0 // 3薪资范围 wageRange
+  jobFrom.value[4] = [] // 4职位类别 jobType
+  jobFrom.value[5] = [] // 5公司行业 business
+  jobFrom.value[6] = [] // 6公司性质 mercNature
+  jobFrom.value[7] = 0 // 7公司规模 mercScale
 
   const setSearchWord = (obj) => {
     searchWord.value = obj
@@ -22,28 +24,14 @@ export const useSearchStore = defineStore('ptsc-search-condition', () => {
 
   // 重设表单条件
   const reset = () => {
-    jobNature.value = []
-    workAge.value = 0
-    jobType.value = []
-    business.value = []
-    mercNature.value = []
-    mercScale.value = 0
-    degree.value = 0
-    wageRange.value = 0
+    jobFrom.value = []
   }
 
   return {
     cityId,
     cityName,
     searchWord,
-    jobNature,
-    workAge,
-    jobType,
-    business,
-    mercNature,
-    mercScale,
-    degree,
-    wageRange,
+    jobFrom,
     setSearchWord,
     reset
   }
--- src/views/Stu/Jobs/components/JobForm.vue
+++ src/views/Stu/Jobs/components/JobForm.vue
@@ -5,40 +5,76 @@ import jobFrom from '@/assets/json/jobFrom'
 import { ArrowUp, ArrowDown } from '@element-plus/icons-vue'
 
 const searchStore = useSearchStore()
-const form = ref({})
+const form = ref([])
 const mousePass = ref([])
 // 初始化表单数据
 onMounted(() => {
-  form.value.degree = searchStore.degree
-  form.value.jobNature = searchStore.jobNature
-  form.value.workAge = searchStore.workAge
-  form.value.jobType = searchStore.jobType
-  form.value.business = searchStore.business
-  form.value.mercNature = searchStore.mercNature
-  form.value.mercScale = searchStore.mercScale
-  form.value.degree = searchStore.degree
-  form.value.wageRange = searchStore.wageRange
+  // 0学历要求1职位性质2工作年限3薪资范围4职位类别5公司行业6公司性质7公司规模
+  form.value = searchStore.jobFrom
 })
+
+// 点击筛选项事件
+const clickInfo = (index1, valueNum) => {
+  console.log('index1: ' + index1 + '///valueNum: ' + valueNum)
+  if (Array.isArray(form.value[index1])) {
+    if (valueNum === 1) {
+      form.value[index1] = []
+      return
+    }
+    if (!form.value[index1].includes(valueNum)) {
+      form.value[index1].push(valueNum)
+    } else {
+      form.value[index1] = form.value[index1].filter((item) => item !== valueNum)
+    }
+  } else {
+    if (valueNum === 1) {
+      form.value[index1] = 0
+    } else {
+      form.value[index1] = valueNum
+    }
+  }
+  console.log('form.value[' + index1 + ']: ' + form.value[index1])
+}
 </script>
 
 <template>
   <div class="job-form">
     <span class="form-title">其他筛选</span>
     <div
-      v-for="(item, index) in jobFrom"
-      :key="index"
+      v-for="(item, index1) in jobFrom"
+      :key="index1"
       class="form-item"
-      @mouseenter="mousePass[index] = true"
-      @mouseleave="mousePass[index] = false"
+      @mouseenter="mousePass[index1] = true"
+      @mouseleave="mousePass[index1] = false"
     >
-      <span>{{ item.title }}</span>
-      <el-icon style="margin-left: 5px">
-        <ArrowDown v-if="!mousePass[index]" />
-        <ArrowUp v-else />
-      </el-icon>
-      <div class="down-infos-box" v-if="mousePass[index]">
-        <div class="down-info-item" v-for="(info, index) in item.info" :key="index">
-          <span>{{ info.label }}</span>
+      <div
+        class="from-item-title"
+        :id="
+          Array.isArray(form[index1])
+            ? form[index1].length === 0
+              ? ''
+              : 'have-been-selected'
+            : form[index1] === 0
+              ? ''
+              : 'have-been-selected'
+        "
+      >
+        <span>{{ item.title }}</span>
+        <el-icon style="margin-left: 5px">
+          <ArrowDown v-if="!mousePass[index1]" />
+          <ArrowUp v-else />
+        </el-icon>
+      </div>
+      <div class="down-all-box">
+        <div class="down-infos-box" v-if="mousePass[index1]">
+          <div
+            class="down-info-item"
+            v-for="(info, index2) in item.info"
+            :key="index2"
+            @click="clickInfo(index1, info.value)"
+          >
+            <span>{{ info.label }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -58,53 +94,69 @@ onMounted(() => {
   .form-item {
     width: 120px;
     height: 40px;
-    font-size: 15px;
-    border-radius: 20px;
     color: #222222;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     margin-left: 5px;
-    background-color: #f5f6f7;
     position: relative;
+    font-size: 15px;
     cursor: pointer;
 
-    .down-infos-box {
+    .from-item-title {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      width: 100%;
+      border-radius: 20px;
+      background-color: #f5f6f7;
+    }
+
+    .down-all-box {
       position: absolute;
       transform-origin: top left;
-      top: 115%;
-      // left: -50%;
+      top: 100%;
       z-index: 997;
-      max-height: 302px;
-      background-color: #fff;
-      border: 1px solid #ebebeb;
-      border-radius: 10px;
-      overflow: auto;
-      padding: 10px;
+      // left: -50%;
+      padding-top: 10px;
+      cursor: auto;
 
-      .down-info-item {
-        min-width: 160px;
-        height: 40px;
+      .down-infos-box {
+        max-height: 302px;
+        background-color: #fff;
+        border: 1px solid #ebebeb;
         border-radius: 10px;
-        line-height: 40px;
-        white-space: nowrap; /* 将文本处理为单行 */
-        overflow: hidden; /* 超出部分隐藏 */
-        text-overflow: ellipsis; /* 使用省略号表示被截断的文本 */
+        overflow: auto;
+        padding: 10px;
 
-        span {
-          margin-left: 10px;
-          margin-right: 10px;
+        .down-info-item {
+          min-width: 160px;
+          height: 40px;
+          border-radius: 10px;
+          line-height: 40px;
+          cursor: pointer;
+          white-space: nowrap; /* 将文本处理为单行 */
+          overflow: hidden; /* 超出部分隐藏 */
+          text-overflow: ellipsis; /* 使用省略号表示被截断的文本 */
+
+          span {
+            margin-left: 10px;
+            margin-right: 10px;
+          }
         }
-      }
 
-      .down-info-item:hover {
-        background-color: #f5f6f7;
+        .down-info-item:hover {
+          background-color: #f5f6f7;
+        }
       }
     }
 
     .down-infos-box::-webkit-scrollbar {
       width: 0px; /* 隐藏滚动条的宽度 */
     }
+  }
+
+  #have-been-selected:not(.down-all-box) {
+    color: #409eff;
+    background-color: #f2fbff;
   }
 }
 </style>
