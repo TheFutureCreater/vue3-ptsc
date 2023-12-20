d7841713c0d4732a7757e998491fcdbe5e8dcfb9
YwonDong <futurecreaterlan@foxmail.com>
2023-12-20 21:50:59 +0800

    职位显示层进一步完善

--- src/views/Stu/Jobs/components/JobList.vue
+++ src/views/Stu/Jobs/components/JobList.vue
@@ -2,6 +2,7 @@
 import { ref, watch, computed } from 'vue'
 import { getJobListService } from '@/api/jobInfo'
 import { useSearchStore } from '@/stores'
+import jobForm from '@/assets/json/jobForm'
 
 const searchStore = useSearchStore()
 const jobList = ref([])
@@ -64,7 +65,29 @@ const handleCurrentChange = () => {
 <template>
   <div class="job-list">
     <el-container v-loading="isloading" class="job-list-container">
-      <div class="job-info-box" v-for="(item, index) in jobList" :key="index"></div>
+      <div class="job-info-box" v-for="(item, index) in jobList" :key="index">
+        <div class="info-for-job">
+          <span class="job-title">{{ item.title }}</span>
+
+          <span class="job-wage">{{
+            (item.minWages > 10 ? item.minWages / 10.0 + '万' : item.minWages + '千') +
+            '-' +
+            (item.maxWages > 10 ? item.maxWages / 10.0 + '万' : item.maxWages + '千')
+          }}</span>
+        </div>
+        <div class="label-for-job">
+          <span v-if="item.nature !== 1">
+            {{ jobForm[0].info[item.nature].label }}
+          </span>
+          <span>{{ jobForm[4].info[item.category].label }}</span>
+          <span>{{ jobForm[2].info[item.workAge].label }}</span>
+          <span>{{ jobForm[1].info[item.degree].label }}</span>
+          <span>{{ '招聘' + item.recruitNum + '人' }}</span>
+        </div>
+        <div class="merchant-title">
+          <el-avatar shape="square" :size="30" :src="item.userAvatar" />
+        </div>
+      </div>
 
       <el-pagination
         v-model:current-page="pageData.pageNum"
@@ -87,6 +110,7 @@ const handleCurrentChange = () => {
     display: flex;
     flex-direction: column;
     align-items: center;
+    min-height: 600px;
 
     .job-info-box {
       width: 100%;
@@ -95,6 +119,45 @@ const handleCurrentChange = () => {
       border-radius: 10px;
       margin-bottom: 20px;
       transition: box-shadow 0.3s ease-in-out;
+      padding: 10px 20px;
+      cursor: pointer;
+
+      .info-for-job {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+
+        .job-title {
+          font-weight: bold;
+          font-size: 20px;
+        }
+
+        .job-wage {
+          font-weight: bold;
+          font-size: 20px;
+          color: #ff9900;
+        }
+      }
+
+      .label-for-job {
+        margin-top: 5px;
+
+        span {
+          display: inline-block;
+          height: 30px;
+          line-height: 30px;
+          padding: 0px 8px;
+          border-radius: 5px;
+          background-color: #f5f6f7;
+          color: #7c8087;
+          font-size: 16px;
+          margin-right: 8px;
+        }
+      }
+
+      .merchant-title {
+        margin-top: 5px;
+      }
     }
 
     .job-info-box:hover {
