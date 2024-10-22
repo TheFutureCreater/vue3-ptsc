24b6c0225792471ef5d2846370995e8a1b95b9ca
YwonDong <futurecreaterlan@foxmail.com>
2024-10-22 10:48:32 +0800

    002

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,9 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
